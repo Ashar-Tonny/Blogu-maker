00324bc20d605d198b4efc597701f185e43438dc
Ashar-Tonny <156561504+Ashar-Tonny@users.noreply.github.com>
2026-04-09 16:04:59 +0300

    Configure static and media file settings

--- Blog/settings.py
+++ Blog/settings.py
@@ -121,16 +121,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_URL = 'static/'
-#STATICFILES_URLS = (
-#    os.path.join(BASE_DIR,'static'),
-#)
+STATIC_URL = 'static/'
+STATICFILES_URLS = (
+  os.path.join(BASE_DIR,'static'),
+)
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Optional: enable compressed static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -138,3 +140,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
